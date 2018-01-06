262b82ba594fd0fea4d8ee11808356d11dee0cc4
Sebastian Iglesias <sebastian.iglesias@mulesoft.com>
2018-01-06 20:39:49 -0300

    SPIKE: Learning Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-products.ts
@@ -0,0 +1,14 @@
+import { Product } from './product';
+
+export const PRODUCTS: Product[] = [
+  { id: 11, name: 'Martillo' },
+  { id: 12, name: 'Taladro' },
+  { id: 13, name: 'Pincel' },
+  { id: 14, name: 'Escalera' },
+  { id: 15, name: 'Cloro' },
+  { id: 16, name: 'Acido' },
+  { id: 17, name: 'Clavo' },
+  { id: 18, name: 'Mesa' },
+  { id: 19, name: 'Silla' },
+  { id: 20, name: 'Mantel' }
+];
